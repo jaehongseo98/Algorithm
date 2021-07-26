b4f3bf39a610f7c577662dca05acffb69908953c
seojaehong <jaehong.seo82@gmail.com>
2021-07-26 16:01:51 +0900

    백준 단계별 문제 (입출력 사칙연산)1 if1

---
+++ BAEKJOON/src/inputoutput/C2588.java
@@ -0,0 +1,35 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+//문제
+//(세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+//
+//
+//
+//(1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
+//
+//출력
+//첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
+public class C2588 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int inputNum1 = sc.nextInt();
+		int inputNum2 = sc.nextInt();
+		int n1 = inputNum1 * (inputNum2 % 10 );
+		int n2 = inputNum1 * (inputNum2 % 100 / 10 );
+		int n3 = inputNum1 * (inputNum2 / 100 );
+		
+		sc.close();
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(inputNum1 * inputNum2);
+	}
+
+}
